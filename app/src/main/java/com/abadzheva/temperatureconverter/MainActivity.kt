186004e3ca1bb2af757df20046c9020070768e3b
k8abadzheva <make.me.loco@gmail.com>
2025-02-11 19:42:45 +0300

    ``` feat: Add launcher icon and initial UI elements
    This commit introduces the following changes:
    
    -   Added a new launcher icon for the Google Play Store (`ic_launcher-playstore.png`).
    -   Added a new background color resource (`ic_launcher_background.xml`) for launcher icons.
    -   Modified the main UI to display "Hello friend!" and "Hello everyone!" messages using Jetpack Compose.
    -   Created a new composable function `Hello` to display a personalized greeting message.
    -   Used `Column` composable for content layout.
    ```

--- app/src/main/java/com/abadzheva/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/abadzheva/temperatureconverter/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text("Hello friend! Welcome to Jetpack Compose.")
+            Column {
+                Hello("friend")
+                Hello("everyone")
+            }
         }
     }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun Hello(name: String) {
+    Text("Hello $name! Welcome to Jetpack Compose.")
 }
