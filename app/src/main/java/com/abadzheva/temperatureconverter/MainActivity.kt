24deac83c525b302eb97a95fc938978edd34e27d
k8abadzheva <make.me.loco@gmail.com>
2025-02-12 16:57:57 +0300

    ``` feat: Implement basic temperature conversion UI
    This commit introduces the initial UI for the temperature converter application using Jetpack Compose.
    
    Key changes:
    
    -   Added a `TemperatureText` composable to display the Celsius to Fahrenheit conversion.
    -   Added a `ConvertButton` composable to trigger the conversion.
    -   Added a `Header` composable to display an image header with a provided description.
    -   Implemented `MainActivityContent` to compose the header, the button and the text.
    -   Added an image resource (`sunrise.webp`) for the header.
    - Implemented a logic that shows the conversion when the `ConvertButton` is clicked.
    - Created a preview function `PreviewMainActivity`.
    ```

--- app/src/main/java/com/abadzheva/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/abadzheva/temperatureconverter/MainActivity.kt
@@ -3,26 +3,77 @@ package com.abadzheva.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Column {
-                Hello("friend")
-                Hello("everyone")
-            }
+            MainActivityContent()
         }
     }
 }
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
-fun Hello(name: String) {
-    Text("Hello $name! Welcome to Jetpack Compose.")
+fun TemperatureText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble() * 9 / 5) + 32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text("Convert")
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun Header(
+    image: Int,
+    description: String,
+) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = description,
+        modifier =
+            Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+        contentScale = ContentScale.Crop,
+    )
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun MainActivityContent() {
+    val celsius = remember { mutableStateOf(0) }
+
+    Column {
+        Header(R.drawable.sunrise, "Sunrise")
+        ConvertButton { celsius.value = 20 }
+        TemperatureText(celsius.value)
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainActivity() {
+    MainActivityContent()
 }
